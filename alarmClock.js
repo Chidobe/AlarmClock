7d7125a554afbbfe4ba79d0ca048bea4ffab8333
Chidobe <thedobecompany@gmail.com>
2022-09-25 13:04:28 +0100

    setting the current time

---
+++ alarmClock.js
@@ -0,0 +1,11 @@
+function setTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const mins = date.getMinutes();
+    const secs = date.getSeconds();
+    const currentTime = `${hours}:${mins}:${secs}`;
+    const time = document.getElementById('time');
+    time.innerText = currentTime;
+};
+
+const interval = setInterval(() => setTime(), 1000);
